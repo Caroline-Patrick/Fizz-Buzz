b9eaff0b03b5dc682157f37092fb4aac8a558d62
Caroline-Patrick <carolinespatrick@gmail.com>
2022-08-01 17:32:22 -0500

    solved fizzbuzz

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Fizz-Buzz
+1 Create a function that lists all numbers between 1 - 1000;
+
+1 but instead of printing the numbers that are evenly divisible by 3, print "fizz";
+
+1 and instead of printing the numbers that are evenly divisible by 5, print "buzz";
+
+1 also, if the number is evenly divisible by both 3 and 5, print "fizzBuzz".
+
+You'll need a few things from your past pre-lessons:
+
+1 % modulus
+
+1 while loop
+
+1 ++ incrementor
+
+1 And something you have covered in depth yet: if(){} if else(){} else {}
+
+
+
---
+++ script.js
@@ -0,0 +1,32 @@
+
+// const countThousand = () => {
+//     let counter = 1;
+//     while (counter < 100) {
+//     console.log(counter);
+//     counter+=3;
+//     }
+// }
+
+// countThousand()
+
+
+// if (counter % 3 == 0) {
+//     let console.log("fizz")
+
+//     if (counter % 5 == 0) {
+//         let console.log("buzz")
+//     }
+// } 
+
+
+
+ for ( i=1; i <1001; i++) {
+    if (i % 3 == 0) console.log("fizz");
+    if (i % 5 == 0) console.log("buzz");
+    if (i % 3 == 0 && i % 5 ==0) console.log("fizzbuzz");
+    else console.log(i);
+
+}
+
+
+console.log(i)
